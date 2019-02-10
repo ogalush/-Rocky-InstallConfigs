62a0c189aa3df478ba3161b3cf8442b1f8671589
ogalush <take_oga@hotmail.com>
2019-02-10 23:47:32 +0900

    チェックコマンド更新

--- controller/home/check_service.sh
+++ controller/home/check_service.sh
@@ -1,5 +1,7 @@
 source ~/admin_openrc.sh
 set -x
+##for i in 'mysql' 'rabbitmq-server' 'memcached' 'etcd' ; do sudo service $i status ; done
 openstack compute service list
 openstack network agent list 
+ip netns
 set +x
