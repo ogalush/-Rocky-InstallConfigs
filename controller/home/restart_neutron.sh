e109a88b074a6193a8b26eadb5a23fe420c0a0e1
ogalush <take_oga@hotmail.com>
2019-02-11 15:24:15 +0900

    Update Restart Script

--- controller/home/restart_neutron.sh
+++ controller/home/restart_neutron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -x
-for i in 'neutron-linuxbridge-agent' 'neutron-dhcp-agent' 'neutron-metadata-agent' 'neutron-l3-agent' 'neutron-server' ; do sudo systemctl restart $i ; done
+for i in 'neutron-server' 'neutron-linuxbridge-agent' 'neutron-dhcp-agent' 'neutron-metadata-agent' 'neutron-l3-agent'; do sudo systemctl restart $i ; done
 set +x
