b450eb36a64018ad57fad3d35bb63f41515a399c
ogalush <take_oga@hotmail.com>
2019-02-11 17:23:42 +0900

    不要デーモンの操作をコメントアウト

--- controller/home/restart_nova.sh
+++ controller/home/restart_nova.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set -x
-sudo service apache2 restart
-sudo service rabbitmq-server stop
-sudo service rabbitmq-server start
-sudo service memcached stop
-sudo service memcached start
+#sudo service apache2 restart
+#sudo service rabbitmq-server stop
+#sudo service rabbitmq-server start
+#sudo service memcached stop
+#sudo service memcached start
 for i in 'nova-api' 'nova-consoleauth' 'nova-scheduler' 'nova-conductor' 'nova-novncproxy' 'nova-compute'; do sudo systemctl restart $i ; done
 set +x
